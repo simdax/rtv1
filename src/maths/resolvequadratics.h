200fde4aada0979422d6be31bb6ad4ac87018074
anlerandy <alerandy@student.fr>
2018-04-10 11:26:26 +0200

    Fixing Quadratics.h typo

---
+++ src/maths/resolvequadratics.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   resolvequadratics.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/03 12:08:41 by scornaz           #+#    #+#             */
+/*   Updated: 2018/04/03 16:32:17 by scornaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RESOLVEQUADRATICS_H
+# define RESOLVEQUADRATICS_H
+
+int	resolve_quadratic(t_vec3f equation, float *solution);
+
+#endif
