8e1f3dd231d21079fa247c40b434db2fe82d67e8
Simon CORNAZ <scornaz@e1r9p14.42.fr>
2018-04-16 15:52:58 +0200

    added libwebsock

--- websocket/install.sh
+++ websocket/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git submodule init && git submodule update
+#git submodule init && git submodule update
 brew install libtool gnu-sed openssl libevent
 gsed -r -i "s/^libtoolize/glibtoolize/" libwebsock/autogen.sh
 gsed -i "s/inline//g" libwebsock/src/utf*
