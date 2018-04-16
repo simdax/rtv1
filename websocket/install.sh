32ae7efb0eec5f9bc9d257ba4fdfbe1f5a97646e
Simon CORNAZ <scornaz@e1r9p14.42.fr>
2018-04-16 15:34:07 +0200

    clean Makefile (build libseocket)

--- websocket/install.sh
+++ websocket/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+git submodule init && git submodule update
 brew install libtool gnu-sed openssl libevent
-gsed -i "s/libtoolize/glibtoolize/" libwesock/autogen.sh
+gsed -r -i "s/^libtoolize/glibtoolize/" libwebsock/autogen.sh
+gsed -i "s/inline//g" libwebsock/src/utf*
