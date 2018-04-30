ed467d8b3349c164115f75c00c64c80531c6fa5c
acourtin <acourtin@student.42.fr>
2018-04-30 12:08:12 +0200

    fix getsdl.sh

--- vendor/install_sdl/get_sdl.sh
+++ vendor/install_sdl/get_sdl.sh
@@ -9,7 +9,7 @@ if [ -f ~/.brew/include/SDL2/SDL.h ] && [ -f ~/.brew/lib/libSDL2.a ];
 	fi
 
 if [ -f ~/.brew/include/SDL2/SDL_image.h ];
-	then printf '-lSDL_image' >> include.dep; echo  '\033[36m[SDL_image Installé]\033[0m';
+	then printf ' -lSDL2_image' >> include.dep; echo  '\033[36m[SDL_image Installé]\033[0m';
 	else if [ -f $ROOT/SDL2_image-2.0.3/libSDL2_image.la ] && [ -f $ROOT/SDL2_image-2.0.3/SDL_image.h ];
 		then printf " -I$ROOT/SDL2_image-2.0.3/ -L$ROOT/SDL2_image-2.0.3/.libs -lSDL2_image" >> include.dep; echo '\033[36m[SDL_image externe Installé]\033[0m';
 		else sh ./vendor/install_sdl/installSDL_img.sh; printf " -I$ROOT/SDL2_image-2.0.3/ -L$ROOT/SDL2_image-2.0.3/.libs/ -lSDL2_image" >> include.dep; echo '\033[1;36m[SDL_image a été installé]\033[0m' ;
@@ -17,7 +17,7 @@ if [ -f ~/.brew/include/SDL2/SDL_image.h ];
 	fi
 
 if [ -f ~/.brew/include/SDL2/SDL_ttf.h ];
-	then printf ' -lSDL_ttf' >> include.dep; echo '\033[36m[SDL_ttf Installé]\n\033[0m';
+	then printf ' -lSDL2_ttf' >> include.dep; echo '\033[36m[SDL_ttf Installé]\n\033[0m';
 	else if [ -f $ROOT/SDL2_ttf-2.0.14/SDL_ttf.h ] && [ -f $ROOT/SDL2_ttf-2.0.14/libSDL2_ttf.la ];
 		then printf " -I$ROOT/SDL2_ttf-2.0.14/ -L$ROOT/SDL2_ttf-2.0.14/.libs/ -lSDL2_ttf" >> include.dep; echo '\033[36m[SDL_ttf externe Installé]\n\033[0m';
 		else sh ./vendor/install_sdl/installSDL_ttf.sh; printf " -I$ROOT/SDL2_ttf-2.0.14/ -L$ROOT/SDL2_ttf-2.0.14/.libs/ -lSDL2_ttf" >> include.dep; echo '\033[1;36m[SDL_ttf a été installé]\n\033[0m' ;
