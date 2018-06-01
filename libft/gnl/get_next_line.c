9b0344132231a60c6dee7714a5c8bef65bba7a2d
anlerandy <alerandy@student.42.fr>
2018-06-02 00:38:10 +0200

    Remplacement de GNL : Reparation de probleme de parsing limite a 35 parse

--- libft/gnl/get_next_line.c
+++ libft/gnl/get_next_line.c
@@ -3,69 +3,143 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: alerandy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/08 15:54:06 by scornaz           #+#    #+#             */
-/*   Updated: 2018/05/10 14:47:37 by scornaz          ###   ########.fr       */
+/*   Created: 2017/11/24 16:07:29 by alerandy          #+#    #+#             */
+/*   Updated: 2018/01/16 10:30:46 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ret_func(int ret, char **stat, char **line)
+static void		delete_stk(t_list **save)
 {
-	if (*stat && ft_strlen(*stat) > 0)
+	t_list		*tmp;
+	t_stock		*stock;
+
+	stock = (*save)->content;
+	if (stock)
 	{
-		*line = *stat;
-		*stat = NULL;
-		return (1);
+		if (stock->str)
+			ft_strdel(&(stock->str));
+		ft_memdel((void *)&stock);
 	}
-	return (ret);
+	stock = NULL;
+	tmp = *save;
+	*save = (*save)->next;
+	ft_memdel((void*)&tmp);
+	tmp = NULL;
 }
 
-static int	copy(char **stat, char **line)
+static void		create_line(const int fd, char **line, t_list **save, int end)
 {
-	char *cpy;
-	char *realloc;
+	t_stock		*stock;
+	int			i;
+	int			start;
 
-	if ((realloc = ft_strchr(*stat, '\n')))
+	i = TRUE;
+	if (*save)
 	{
-		cpy = ft_strdup(realloc + 1);
-		*line = (char*)ft_memalloc(sizeof(char) * (realloc - *stat + 1));
-		ft_bzero(*line, realloc - *stat + 1);
-		ft_memcpy(*line, *stat, (realloc - *stat));
-		free(*stat);
-		*stat = cpy;
-		return (1);
+		stock = (*save)->content;
+		if (stock->fd == fd && (stock->done == TRUE || end == 1) && !(*line))
+		{
+			start = stock->index;
+			while (stock->len > stock->index && i == TRUE)
+				i = (stock->str[stock->index++] != '\n');
+			if (i == FALSE || end == 1)
+				*line = ft_strndup(stock->str, stock->index - start - !i);
+			else
+			{
+				stock->done = 0;
+				return ;
+			}
+		}
+		else
+			create_line(fd, line, &((*save)->next), 0);
+		(stock->index == stock->len ? delete_stk(&(*save)) : (stock->done = 0));
+	}
+}
+
+static int		join_read(const int fd, t_read *red, t_stock **stock, int *eof)
+{
+	if (red->ret == 0 && !(*stock))
+		return ((*eof = TRUE));
+	if (!(*stock))
+		if (!(*stock = ft_memalloc(sizeof(t_stock))))
+			return (-1);
+	(*stock)->fd = fd;
+	if ((*stock)->str && (*stock)->done == FALSE)
+	{
+		free(red->buffer);
+		if (!(red->buffer = ft_strndup((*stock)->str + (*stock)->index,
+						(*stock)->len - (*stock)->index)))
+			return (-1);
+		(*stock)->index = 0;
+		ft_strdel(&((*stock)->str));
+	}
+	(!red->buffer ? (red->buffer = ft_memalloc(1)) : red->buffer);
+	if (!((*stock)->str = ft_strnew(red->ret + (*stock)->len + 1)))
+		return (-1);
+	(*stock)->str = ft_strcpy((*stock)->str, red->buffer);
+	(*stock)->str = ft_strncat((*stock)->str, red->buffed, red->ret);
+	(*stock)->len = ft_strlen((*stock)->str);
+	ft_strdel(&(red->buffer));
+	while (*eof < (*stock)->len)
+		(((*stock)->str)[(*eof)++] == '\n' ? ((*stock)->done = TRUE) : *eof);
+	return ((*eof = 0));
+}
+
+static int		stock_line(const int fd, t_read *red, t_list **save, int *eof)
+{
+	t_stock		*stock;
+
+	stock = NULL;
+	if (!(*save))
+	{
+		if (join_read(fd, red, &stock, eof) < 0)
+			return (-1);
+		if (stock)
+		{
+			*save = ft_lstnew(stock, sizeof(t_stock));
+			ft_memdel((void *)&stock);
+		}
+	}
+	else
+	{
+		stock = (*save)->content;
+		if (!stock)
+			return (stock_line(fd, red, &((*save)->next), eof));
+		if (fd != stock->fd)
+			return (stock_line(fd, red, &((*save)->next), eof));
+		if (join_read(fd, red, &stock, eof) < 0)
+			return (-1);
 	}
 	return (0);
 }
 
-int			get_next_line(int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	static char	*stat[MAX_FD];
-	char		buf[BUFF_SIZE + 1];
-	char		*realloc;
-	int			ret;
-	int			len;
+	static t_list	*save = NULL;
+	int				eof;
+	t_read			red;
 
-	if (!line || fd < 0 || fd > MAX_FD)
+	eof = FALSE;
+	if (!line || BUFF_SIZE < 1)
 		return (-1);
-	if (stat[fd] && ft_strlen(stat[fd]) > 0 && copy(&stat[fd], line))
-		return (1);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	*line = NULL;
+	red.buffer = NULL;
+	while (!(*line) && eof == FALSE)
 	{
-		buf[ret] = 0;
-		len = stat[fd] ? ft_strlen(stat[fd]) : 0;
-		realloc = (char*)ft_memalloc(sizeof(char) * (len + ret + 1));
-		ft_bzero(realloc, len + ret + 1);
-		if (stat[fd])
-			ft_memcpy(realloc, stat[fd], len);
-		ft_memcpy(realloc + len, buf, ret);
-		free(stat[fd]);
-		stat[fd] = realloc;
-		if (copy(&stat[fd], line))
-			return (1);
+		create_line(fd, line, &save, 0);
+		if (!(*line) && eof == FALSE)
+		{
+			if ((red.ret = read(fd, red.buffed, BUFF_SIZE)) < 0)
+				return (-1);
+			if (stock_line(fd, &red, &save, &eof) < 0)
+				return (-1);
+		}
+		if (!*line && eof == FALSE)
+			(red.ret == 0 ? create_line(fd, line, &save, 1) : (red.ret = 0));
 	}
-	return (ret_func(ret, &stat[fd], line));
+	return (*line != NULL);
 }
--- libft/gnl/get_next_line.h
+++ libft/gnl/get_next_line.h
@@ -3,27 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: alerandy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/15 10:53:07 by scornaz           #+#    #+#             */
-/*   Updated: 2018/05/09 14:58:22 by scornaz          ###   ########.fr       */
+/*   Created: 2017/11/24 16:02:13 by alerandy          #+#    #+#             */
+/*   Updated: 2017/12/11 17:36:11 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# define BUFF_SIZE		64
-# define MAX_FD				100
-
-# include <sys/types.h>
-# include <limits.h>
-# include <stdio.h>
-# include "../libft.h"
-# include "unistd.h"
+# include "libft.h"
 # include <fcntl.h>
+# include <unistd.h>
 
-int			get_next_line(int fd, char **line);
+# define BUFF_SIZE 32
+# define FALSE 0
+# define TRUE 1
+
+typedef struct	s_read
+{
+	char		buffed[BUFF_SIZE];
+	char		*buffer;
+	int			ret;
+}				t_read;
+
+typedef struct	s_stock
+{
+	char		*str;
+	int			done;
+	int			index;
+	int			len;
+	int			fd;
+}				t_stock;
+
+int				get_next_line(const int fd, char **line);
 char		*get_file_comment(char *path, char com_char);
-
 #endif
