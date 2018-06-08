038c361a1f08d5f85dc142d66eea1b16d6e3c5cf
Andy LERANDY <alerandy@student.42.fr>
2018-06-08 13:01:29 +0200

    RT : Ajout du systême de rotation à la caméra.

--- src/render/sdl2.c
+++ src/render/sdl2.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <acourtin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 16:33:25 by acourtin          #+#    #+#             */
-/*   Updated: 2018/06/07 17:04:49 by alerandy         ###   ########.fr       */
+/*   Updated: 2018/06/08 12:58:25 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static void	change_scene(t_render_opts *opts)
 	}
 }
 
-static void	key_event(t_render_opts *opts, t_sdl *sdl)
+static void	cam_move(t_sdl *sdl, t_render_opts *opts)
 {
 	if (sdl->event->key.keysym.sym == SDLK_DOWN)
 		opts->camorig.z += 1;
@@ -41,14 +41,23 @@ static void	key_event(t_render_opts *opts, t_sdl *sdl)
 	else if (sdl->event->key.keysym.sym == SDLK_LEFT)
 		opts->camorig.x -= 1;
 	else if (sdl->event->key.keysym.sym == SDLK_KP_8)
-		opts->camdir.y += 0.1;
+		ft_rot_x(&opts->camdir.y, &opts->camdir.z, ASPEED);
 	else if (sdl->event->key.keysym.sym == SDLK_KP_5)
-		opts->camdir.y -= 0.1;
+		ft_rot_x(&opts->camdir.y, &opts->camdir.z, -ASPEED);
 	else if (sdl->event->key.keysym.sym == SDLK_KP_6)
-		opts->camdir.x += 0.1;
+		ft_rot_y(&opts->camdir.x, &opts->camdir.z, -ASPEED);
 	else if (sdl->event->key.keysym.sym == SDLK_KP_4)
-		opts->camdir.x -= 0.1;
-	else if (sdl->event->key.keysym.sym == SDLK_a)
+		ft_rot_y(&opts->camdir.x, &opts->camdir.z, ASPEED);
+	else if (sdl->event->key.keysym.sym == SDLK_KP_9)
+		ft_rot_z(&opts->camdir.x, &opts->camdir.y, -ASPEED);
+	else if (sdl->event->key.keysym.sym == SDLK_KP_3)
+		ft_rot_z(&opts->camdir.x, &opts->camdir.y, ASPEED);
+}
+
+static void	key_event(t_render_opts *opts, t_sdl *sdl)
+{
+	cam_move(sdl, opts);
+	if (sdl->event->key.keysym.sym == SDLK_a)
 		change_scene(opts);
 	else if (sdl->event->key.keysym.sym == SDLK_s)
 		snap_screen(opts, sdl);
