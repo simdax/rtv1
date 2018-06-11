534e81346b17b6540240e01e01d4ccc1cd7cf5d7
Andy LERANDY <alerandy@student.42.fr>
2018-06-11 13:10:46 +0200

    LIBFT : Retour au précédent GNL

--- libft/gnl/get_next_line.h
+++ libft/gnl/get_next_line.h
@@ -3,39 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alerandy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/24 16:02:13 by alerandy          #+#    #+#             */
-/*   Updated: 2018/06/09 14:30:04 by alerandy         ###   ########.fr       */
+/*   Created: 2017/11/15 10:53:07 by scornaz           #+#    #+#             */
+/*   Updated: 2018/06/11 13:09:45 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include "libft.h"
+
+# define BUFF_SIZE		1024
+# define MAX_FD			2048
+
+# include <sys/types.h>
+# include <limits.h>
+# include <stdio.h>
+# include "../libft.h"
+# include "unistd.h"
 # include <fcntl.h>
-# include <unistd.h>
 
-# define BUFF_SIZE 2048
-# define FALSE 0
-# define TRUE 1
+int			get_next_line(int fd, char **line);
+char		*get_file_comment(char *path, char com_char);
 
-typedef struct	s_read
-{
-	char		buffed[BUFF_SIZE];
-	char		*buffer;
-	int			ret;
-}				t_read;
-
-typedef struct	s_stock
-{
-	char		*str;
-	int			done;
-	int			index;
-	int			len;
-	int			fd;
-}				t_stock;
-
-int				get_next_line(const int fd, char **line);
-char			*get_file_comment(char *path, char com_char);
 #endif
